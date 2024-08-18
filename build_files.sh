6c889efe025fb1131a92f32072829ddfa796b026
si.okpeku@otandtconsulting.com <tempdev07@swglobal.com>
2024-08-18 18:15:43 +0100

    update

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+# Create virtual environment if not exists
+python -m venv venv
+
+# Activate the virtual environment
+source virtualenv\Scripts\activate 
+
+# Install dependencies
+
 pip install -r requirements.txt
 python3.12 manage.py collectstatic
