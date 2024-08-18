361d08109a1faa20f40a3990d960f4276925328b
si.okpeku@otandtconsulting.com <tempdev07@swglobal.com>
2024-08-18 18:24:03 +0100

    update

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,11 @@
-# Create virtual environment if not exists
-python -m venv venv
+#!/bin/bash
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
-# Activate the virtual environment
-source venv/bin/activate
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-# Install dependencies
-
-pip install -r requirements.txt
-python3.12 manage.py collectstatic
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
