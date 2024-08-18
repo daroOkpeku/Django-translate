0d84da15fa665d738cb7b0c04dfcf1f798fffb6e
si.okpeku@otandtconsulting.com <tempdev07@swglobal.com>
2024-08-18 18:59:06 +0100

    update

--- my_app/settings.py
+++ my_app/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
