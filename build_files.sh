aa2917e6d7f197dde30e2e113f7052c6a269bc62
si.okpeku@otandtconsulting.com <tempdev07@swglobal.com>
2024-08-18 17:57:20 +0100

    update

--- build_files.sh
+++ build_files.sh
@@ -1,23 +1,2 @@
-#!/bin/bash
-
-# Step 1: Install dependencies
-echo "Installing dependencies..."
 pip install -r requirements.txt
-
-# Step 2: Run Django migrations
-echo "Running migrations..."
-python manage.py migrate
-
-# Step 3: Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear --no-post-process
-
-# Step 4: Any other commands you need (e.g., building front-end assets)
-# For example, if you have a React front end:
-echo "Building front-end assets..."
-npm install
-npm run build
-
-# Step 5: Other necessary steps (e.g., compiling messages, running tests, etc.)
-echo "Compiling messages..."
-python manage.py compilemessages
+python3.12 manage.py collectstatic
